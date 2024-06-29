110ca403260f68c330b615dfd743a9d251bafd66
alexf <allthelinks152@gmail.com>
2024-06-29 23:57:57 +0200

    Added a Pine Cone (fuel), custom tooltips and custom Modtags

--- src/main/java/net/squirrel/tutorialmod/item/ModCreativeModTabs.java
+++ src/main/java/net/squirrel/tutorialmod/item/ModCreativeModTabs.java
@@ -23,6 +23,7 @@ public class ModCreativeModTabs {
                 pOutput.accept(ModItems.METAL_DETECTOR.get());
 
                 pOutput.accept(ModItems.STRAWBERRY.get());
+                pOutput.accept(ModItems.PINE_CONE.get());
 
                 pOutput.accept(ModBlocks.SAPPHIRE_BLOCK.get());
                 pOutput.accept(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
